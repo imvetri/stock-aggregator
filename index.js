9f9c037ab835164510ac3d540dbcfa20d9598b8c
imvetri <svetrivel.91@gmail.com>
2017-07-16 08:44:01 +0530

    route to public folder

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.use(express.static('public'))
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
